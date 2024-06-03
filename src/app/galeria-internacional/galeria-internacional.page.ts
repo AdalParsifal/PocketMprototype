39600e57f072c07f1a3c5d14a940961d857c9829
Perceval <albertobustac@gmail.com>
2024-06-02 21:04:19 -0400

    G_internacionales_esqueleto

--- src/app/galeria-internacional/galeria-internacional.page.ts
+++ src/app/galeria-internacional/galeria-internacional.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galeria-internacional.page.scss'],
 })
 export class GaleriaInternacionalPage implements OnInit {
+  novedades = [
+    { image: 'assets/images/novedad1.jpg', title: 'Novedad 1' },
+    { image: 'assets/images/novedad2.jpg', title: 'Novedad 2' },
+    { image: 'assets/images/novedad3.jpg', title: 'Novedad 3' },
+    { image: 'assets/images/novedad4.jpg', title: 'Novedad 4' }
+  ];
+
+  galerias = [
+    'Galería 1',
+    'Galería 2',
+    'Galería 3',
+    'Galería 4',
+    'Galería 5',
+    'Galería 6'
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  filterGalerias(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+    this.galerias = this.galerias.filter(galeria => galeria.toLowerCase().includes(searchTerm));
+  }
 }
