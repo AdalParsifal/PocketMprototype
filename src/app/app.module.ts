69488124e0db1e555bc305068923cbae226fc9b4
Perceval <albertobustac@gmail.com>
2024-06-03 00:17:40 -0400

    G_nacional construccion y swiper install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- src/app/galeria-nacional/galeria-nacional.page.ts
+++ src/app/galeria-nacional/galeria-nacional.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicSlides } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-galeria-nacional',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galeria-nacional.page.scss'],
 })
 export class GaleriaNacionalPage implements OnInit {
+  slides = [
+    { image: 'assets/images/art1.jpg', title: 'Artwork 1' },
+    { image: 'assets/images/art2.jpg', title: 'Artwork 2' },
+    { image: 'assets/images/art3.jpg', title: 'Artwork 3' }
+  ];
+  swiperModules = [IonicSlides];
 
   constructor() { }
 
