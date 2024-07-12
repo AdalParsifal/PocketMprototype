1712bac7a22f9fb265bbb7583240b27c8051a04d
Perceval <albertobustac@gmail.com>
2024-07-11 22:00:08 -0400

    specs para jasmine 1

--- src/app/galeria-nacional/galeria-nacional.page.spec.ts
+++ src/app/galeria-nacional/galeria-nacional.page.spec.ts
@@ -1,11 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GaleriaNacionalPage } from './galeria-nacional.page';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IonTitle, IonicModule } from '@ionic/angular'; // Importa IonTitle e IonicModule de Ionic
 
 describe('GaleriaNacionalPage', () => {
   let component: GaleriaNacionalPage;
   let fixture: ComponentFixture<GaleriaNacionalPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [GaleriaNacionalPage],
+      imports: [HttpClientTestingModule, IonicModule.forRoot()], // Agrega IonicModule.forRoot() aquí si es necesario
+      providers: [], // No es necesario el proveedor de IpGeolocationService para esta prueba
+    }).compileComponents();
+
     fixture = TestBed.createComponent(GaleriaNacionalPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -13,5 +21,21 @@ describe('GaleriaNacionalPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    const compiled = fixture.nativeElement;
+    const ionTitle = compiled.querySelector('ion-title');
+    expect(ionTitle.textContent).toContain('Galeria Nacional');
+  });
+
+  // Puedes añadir más pruebas según sea necesario
+
+  afterEach(() => {
+    // Limpia después de cada prueba si es necesario
+  });
+
+  afterAll(() => {
+    // Limpia después de todas las pruebas si es necesario
   });
 });
--- src/app/services/ip-geolocation.service.spec.ts
+++ src/app/services/ip-geolocation.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IpGeolocationService } from './ip-geolocation.service';
 
 describe('IpGeolocationService', () => {
   let service: IpGeolocationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Importa HttpClientModule aquí si es necesario
+      providers: [IpGeolocationService],   // Asegúrate de proporcionar tu servicio aquí si no está en tu módulo
+    });
     service = TestBed.inject(IpGeolocationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  // Puedes añadir más pruebas según sea necesario
+
+  afterEach(() => {
+    // Limpia después de cada prueba si es necesario
+  });
+
+  afterAll(() => {
+    // Limpia después de todas las pruebas si es necesario
   });
 });
